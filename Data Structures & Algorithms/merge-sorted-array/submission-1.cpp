4b99b534a0a35fcf74db06978d75d125eb2e79f1
Mubashir sikandar <mubashirsikandarpatoli@gmail.com>
2026-07-16 03:51:27 -0700

    Add: merge-sorted-array - submission-1

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> out;
+
+        int i = 0, j = 0;
+
+        // merge only valid parts
+        while(i < m && j < n) {
+            if(nums1[i] <= nums2[j]) {
+                out.push_back(nums1[i]);
+                i++;
+            } else {
+                out.push_back(nums2[j]);
+                j++;
+            }
+        }
+
+        // remaining elements
+        while(i < m) {
+            out.push_back(nums1[i]);
+            i++;
+        }
+
+        while(j < n) {
+            out.push_back(nums2[j]);
+            j++;
+        }
+
+        // copy back to nums1
+        for(int k = 0; k < m + n; k++) {
+            nums1[k] = out[k];
+        }
+    }
+};
